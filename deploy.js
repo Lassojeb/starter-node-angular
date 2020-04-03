8a444c91882e6ceafa3ed6178d5a5ea2eb39601b
Jeb <lassojeb@gmail.com>
2020-04-02 22:51:26 -0600

    update travis

--- deploy.js
+++ deploy.js
@@ -87,7 +87,7 @@ function sshConnect() {
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
       host: '34.228.231.74',
       username: 'ubuntu',
-      privateKey: 'hs-key.pem'
+      privateKey: 'final-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
